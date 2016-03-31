0e4b2e62b644eb085037efb13a67cebb85f4227b
Mateusz Leonowicz <leonwonziu@gmail.com>
2016-03-31 23:10:48 +0200

    Init commit and Build

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour
+{
+
+    public int columns = 5;
+    public int rows = 5;
+
+    public GameObject tile;
+    public Transform BoardParent;
+    public Texture2D source;
+    public List<Tile> Grids = new List<Tile>();
+
+    public byte[] result;
+
+    public void Start()
+    {
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "source.jpg");
+
+        result = System.IO.File.ReadAllBytes(filePath);
+        source = new Texture2D(50, 50);
+        source.LoadImage(result);
+        SetupScene();
+    }
+
+    public void SetupBoard()
+    {
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                GameObject instance = Instantiate(tile, new Vector2(i, j), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(BoardParent);
+                instance.name = i + " x " + j;
+                Grids.Add(instance.GetComponent<Tile>());
+                instance.GetComponent<SpriteRenderer>().color = GetColorPerPixel(i, j);
+                instance.GetComponent<Animator>().SetTrigger(Random.Range(1, 5).ToString());
+            }
+        }
+    }
+
+
+
+    public void SetupScene()
+    {
+        SetupBoard();
+        //BoardParent.transform.position = new Vector2(-columns / 2, -rows / 2);
+    }
+    public Color GetColorPerPixel(int x, int y)
+    {
+        return source.GetPixel(x, y);
+    }
+    public Color GetColorPerPixel(Vector2 pos)
+    {
+        return source.GetPixel((int)pos.x, (int)pos.y);
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using System.Text;
+[Serializable]
+public class Tile : MonoBehaviour
+{
+    public Vector2 position;
+
+    public SpriteRenderer sr;
+    public void Start()
+    {
+        position = transform.localPosition;
+        sr = GetComponent<SpriteRenderer>();
+     //   StartCoroutine(ChangeColor());
+    }
+
+    public IEnumerator ChangeColor()
+    {
+        while (true)
+        {
+            Color32 nc = new Color32((byte)UnityEngine.Random.Range(0, 255), (byte)UnityEngine.Random.Range(0, 255), (byte)UnityEngine.Random.Range(0, 255), 255);
+            sr.color = nc;
+            yield return new WaitForSeconds(UnityEngine.Random.Range(1, 4));
+
+        }
+    }
+
+}
+
